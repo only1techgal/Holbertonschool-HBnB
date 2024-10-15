f984c793914ff577f6831a662bd31c4b33fbab9a
only1techgal <jenn.lipholo@gmail.com>
2024-10-15 17:33:29 +0200

    structure

---
+++ part2/services/facade.py
@@ -0,0 +1,18 @@
+from app.persistence.repository import InMemoryRepository
+
+class HBnBFacade:
+    def __init__(self):
+        self.user_repo = InMemoryRepository()
+        self.place_repo = InMemoryRepository()
+        self.review_repo = InMemoryRepository()
+        self.amenity_repo = InMemoryRepository()
+
+    # Placeholder method for creaating a user
+    def create_usr(self, usr_data):
+        # Logic will be implemented in later tasks
+        pass
+
+    # Placeholder method for fetching a place by ID
+    def get_place(self, place_id):
+         # Logic will be implemented in later tasks
+        pass
